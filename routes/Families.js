478022ea502d577ab6ccc07d3c0ed1a42dcc1120
unknown <satheesh.s@ecgroup-intl.com>
2019-02-22 11:47:24 +0530

    Organization works updated

--- routes/Families.js
+++ routes/Families.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const url = require('url');
+const querystring = require('querystring');
 const axios = require('axios');
 const app = express();
 var extServerOptionsPost;
 const familyRouter = express.Router();
 
 familyRouter.route('/').get(function (req, res) {
-    console.log()
+    let url =  'http://parishsoftcustomerapi.azurewebsites.net/api/Families/' + '?limit='+ req.query.limit + '&offset=' + req.query.offset;
+     if (req.query.firstname) {
+          url = url + '&firstname=' + req.query.firstname;
+        } else {
+          url = url;
+        }
+   if (req.query.lastname) {
+      url = url + '&lastname=' + req.query.lastname;
+    } else {
+      url = url;
+    }
+    if (req.query.quicksearch) {
+      url = url + '&quicksearch=' + req.query.quicksearch;
+    } else {
+      url = url;
+    }
+    if (req.query.familyduid) {
+      url = url + '&familyduid=' + req.query.familyduid;
+    } else {
+      url = url;
+    }
     axios({
         method: 'get',
-        url: 'http://parishsoftcustomerapi.azurewebsites.net/api/Families',
+        url: url,
         headers: {
             'Content-Type': 'application/json',
             'Authorization': req.headers.authorization
