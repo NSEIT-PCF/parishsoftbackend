200c1194c2edcf923ebb54a042ca98cc53ea19ba
satheeshkumar <satheeshkumars@nseit.com>
2019-11-07 15:56:50 +0530

    Code updated for PCF

--- routes/Authenticat.js
+++ routes/Authenticat.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const authenticateRouter = express.Router();
-let PostBody
+let PostBody;
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
 
 let extServerOptionsPost;
 authenticateRouter.route('/').post(function (req, res) {
@@ -18,10 +20,14 @@ authenticateRouter.route('/').post(function (req, res) {
             'Content-Type': 'application/json'
         },
     };
+    localStorage.setItem('ApiKey', PostBody.ApiKey);
     axios.post(extServerOptionsPost.host, {
         ApiKey: PostBody.ApiKey
     }).then(function (response) {
         res.send(response.data);
+        localStorage.setItem('bearer', response.data);
+        console.log(localStorage.getItem('ApiKey'), localStorage.getItem('bearer'));
+
     }).catch(function (error) {
         res.send(error);
     });
